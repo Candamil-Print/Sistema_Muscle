bf41d5e13b9138f099575151ee08ad3ca2faf3f0
Samuel Monsalve <monsalvegomezsamuel2@gmail.com>
2026-02-22 00:16:28 -0500

    feat: set up initial Express application and database connection.

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,22 @@
 require('dotenv').config();
-const express = require('express');
+const app = require('./src/app');
 const pool = require('./config/db');
 
-const app = express();
-app.use(express.json());
-
-app.get('/test-db', async (req, res) => {
+async function connectDB() {
   try {
-    const result = await pool.query('SELECT NOW()');
-    res.json({
-      success: true,
-      time: result.rows[0],
-    });
+    const client = await pool.connect();
+    await client.query('SELECT NOW()');
+    console.log('Successfully connected with NEON');
+    client.release();
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false });
+    console.error('Error connecting with NEON', error.message);
+    process.exit(1);
   }
-});
-
-
-async function connectDB () {
-    try {
-        const client = await pool.connect();
-        const result = await client.query('SELECT NOW()');
-        console.log('Successful Conction');
-        console.log('The server time is:', result.rows[0].now);
-        client.release();
-    } catch (error) {
-        console.error('Error connecting to the database:', error.message);
-        process.exits(1);
-    } 
 }
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, async () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
   await connectDB();
 });
